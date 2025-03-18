c13ca79f19aab5f311bb6b2b1394ad12e38626e7
thanhhoa61 <hoa132aa@gmail.com>
2025-03-18 10:15:19 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('aiArticleLink').addEventListener('click', function() {
+    document.getElementById('articleTitle').innerText = 'Bài viết về Trí tuệ nhân tạo';
+    document.getElementById('articleContent').innerText = 'Trí tuệ nhân tạo (AI) là công nghệ...';
+});
+
+document.getElementById('jsArticleLink').addEventListener('click', function() {
+    document.getElementById('articleTitle').innerText = 'Bài viết về Object trong Javascript';
+    document.getElementById('articleContent').innerText = 'Trong Javascript, Object là một kiểu dữ liệu...';
+});
